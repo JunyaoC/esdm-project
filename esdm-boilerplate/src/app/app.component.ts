33d63838269e5fcd7147b1a9a82842491c7a1faf
Junyao Chan <junyaoxiandingchan@gmail.com>
2021-05-20 13:02:00 +0800

    create storage

--- esdm-boilerplate/src/app/app.component.ts
+++ esdm-boilerplate/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
-import { NavController } from '@ionic/angular';
 import { CoreServiceService } from './core-service.service';
 
 @Component({
@@ -12,20 +11,8 @@ export class AppComponent {
   
   constructor(
     private storage: Storage,
-    public navCtrl: NavController,
     public cs:CoreServiceService
     ) {
-
-    this.storage.create();
-    this.storage.get('storage_xxx').then((res:any) => {
-
-      if(res == null) {
-        this.navCtrl.navigateRoot('/login-page');
-      } else {
-        this.cs.currentRole = res;
-        this.navCtrl.navigateRoot('/home-page');
-      }
-    })
 
   }
 }
